42be5c262c2846da7b8b75938fe3e4980c46d795
hayato takemoto <takebnn26@gmail.com>
2016-10-24 14:23:41 +0900

    created function of posting messages
    setting routing of messages/create
    setting create action in messages_controller
    splitting the application.html.haml to messages/index.html.haml

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,25 @@
 class MessagesController < ApplicationController
 
   def index
+    @messages = Message.all
+    @message = Message.new
   end
+
+  def create
+    message = Message.new(message_params)
+
+    if message.save(message_params)
+      redirect_to messages_path
+    else
+      render root_path
+    end
+
+  end
+
+  private
+
+   def message_params
+    params.require(:message).permit(:content)
+   end
 
 end
